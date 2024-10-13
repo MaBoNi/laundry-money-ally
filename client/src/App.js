10b969318ba9efe7400ae46524296f60ee167537
Martin Bonde <martin@maboni.dk>
2024-10-13 19:39:54 +0200

    Refactor button styling and layout in App component

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="hello-world-box">
-        Naja
+      <div className="button-container">
+        <button className="blue-button">NOAH</button>
+        <button className="blue-button">NAJA</button>
       </div>
-      <div className="hello-world-box">
-        Noah
+      <div className="button-container">
+        <button className="blue-button large-button">FAR</button>
       </div>
     </div>
-    
   );
 }
 
