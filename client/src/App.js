17361efafdd9321b01b62a2db58965daf1fa8392
Martin Bonde <martin@maboni.dk>
2024-10-13 17:54:41 +0200

    This project was bootstrapped with "Create React App"

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="hello-world-box">
+        Naja
+      </div>
+      <div className="hello-world-box">
+        Noah
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
